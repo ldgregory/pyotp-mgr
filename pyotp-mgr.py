8f994d1e59a9bd3f9ecf1c906a4b6e736bd2461b
Leif Gregory <leif@devtek.org>
2021-02-10 20:06:29 -0700

    Added license

--- pyotp-mgr.py
+++ pyotp-mgr.py
@@ -30,6 +30,20 @@ Generate and display QR codes for TOTP while also saving them in encrypted forma
 20190818 -  Added --rebuild feature and Fernet key generation as a file or with
             --key option, added DB functions for TOTP storage.
 20190816 -  Initial code
+
+Copyright 2020 Leif Gregory
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
 """
 
 import argparse
